cd231cf442700cda19969d58bece3c97335e593a
diego825327@gmail.com <diego825327@gmail.com>
2022-01-27 21:34:15 -0300

    Primeiros exercicios

---
+++ Exercicio01Java.java
@@ -0,0 +1,15 @@
+package br.com.generation.repeticao.exercicios;
+
+public class Exercicio01Java {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1000; i < 2000; i++) {
+			
+			if(i % 11 == 5) {
+				System.out.println("Valores divididos por 11 que o resto é 5:" + i);
+			}
+		}
+	}
+		
+}
---
+++ Exercicio04.java
@@ -0,0 +1,31 @@
+package br.com.generation.repeticao.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		int num;
+		double raiz, potencia;
+		
+		System.out.println("Digite um número inteiro: ");
+		num = leia.nextInt();
+		
+		if(num % 2 ==0) {
+			System.out.println("Par...");
+			raiz = Math.sqrt(num);
+			System.out.println("Raiz quadrada: " + raiz);
+		}
+		else {
+			System.out.println("Impar...");
+			potencia = Math.pow(num, 2);
+			System.out.println("Potencia: " + potencia);
+		
+
+		}
+	}
+}
+	
+
